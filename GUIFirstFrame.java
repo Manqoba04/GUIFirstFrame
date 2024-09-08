721498496bb2da955f8a5b157e694936aef3a3f1
Manqoba04 <140062598+Manqoba04@users.noreply.github.com>
2024-09-08 02:12:59 +0200

    Add files via upload
    This application shows the basic GUI frame

---
+++ GUIFirstFrame.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut.ui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author sifu
+ */
+public class GUIFirstFrame extends JFrame {
+    public GUIFirstFrame(){
+      setTitle("Manqoba Nkosi");
+      setSize(200,300);
+      setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultLookAndFeelDecorated(true);
+        setForeground(Color.red);
+        setVisible(true);
+        
+    
+    }
+    
+}
